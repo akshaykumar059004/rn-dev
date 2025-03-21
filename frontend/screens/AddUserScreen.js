1d772dca63d0ddb22329922b4536e13661c198ae
Akshay <akshaykumar059004@gmail.com>
2025-03-21 17:06:29 +0530

    Fixed API Url

--- frontend/screens/AddUserScreen.js
+++ frontend/screens/AddUserScreen.js
@@ -8,7 +8,7 @@ export default function AddUserScreen({ navigation }) {
 
   const handleSubmit = async () => {
     try {
-      await axios.post("https://rn-dev.onrender.com/users", { name, email });
+      await axios.post("https://rn-dev.onrender.com/api/users", { name, email });
       alert("User added!");
       navigation.goBack();
     } catch (error) {
